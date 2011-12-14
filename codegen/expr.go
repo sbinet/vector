6e77055c8344ad462b3175914dc566ebcbef5f8b
Samuel C. Payson <scpayson@gmail.com>
2011-12-14 02:19:54 -0500

    Added more sophisticated expression system.
    Expressions are cached and can be uniquely identified with a single
    integer comparison. The system is pretty badass if you ask me =].
    This will let us search much deeper without hitting memory
    constraints. It's still just a constant speedup, but it's not bad.

--- codegen/expr.go
+++ codegen/expr.go
@@ -1,55 +1,133 @@
 package codegen
 
-//import "astar"
+// Uses bit-shifty tricks to map the labels of two nodes to their parent
+type ExprDB struct {
+   // Look-Up-Table for expressions
+   lut map[int]map[int][]int
 
+   // Actual list of expressions
+   list []Expr
+}
+
+func NewExprDB() *ExprDB {
+   return &ExprDB{
+      lut:  make(map[int]map[int][]int),
+      list: make([]Expr, 1, 256)}
+}
+
+// Expressions made up of binary operations
 type Expr interface {
-   Equiv(e Expr) bool
+   // How many operations are required to calculate this expression
+   NumOps() int
+
+   // Labels uniquely identify expressions based on what they compute. Nodes
+   // with equivalent labels compute equivalent sums/products
+   Label() int
 }
 
-// Operands are identified by a unique integer
-type Operand int
+const (
+   OpAdd = 0
+   OpMul = 1
+)
 
-func (me *Operand) Equiv(e Expr) bool {
-   op, ok := e.(*Operand)
-   if ok {
-      return int(*op) == int(*me)
-   } else {
-      return false
-   }
+type Literal int
+
+func (me *ExprDB) NewLiteral() int {
+   rv := len(me.list)
+   me.list = append(me.list, Literal(rv))
+   return rv
 }
 
+func (me Literal) NumOps() int {
+   return 0
+}
+
+func (me Literal) Label() int {
+   return int(me)
+}
+
+// Represents a multiplication
 type Mul struct {
-   left, right Expr
+   db                *ExprDB
+   left, right, self int
 }
 
-func (me *Mul) Equiv(e Expr) bool {
-   mul, ok := e.(*Mul)
-   if ok {
-      a := mul.left.Equiv(me.right) && mul.right.Equiv(me.left)
-      b := mul.left.Equiv(me.left) && mul.right.Equiv(me.right)
+func (me *ExprDB) NewMul(left, right int) int {
 
-      return a || b
-   } else {
-      return false
+   if left > right {
+      left, right = right, left
    }
 
-   panic("bad return path error (x_X)")
+   mp := me.lut[left]
+   if mp == nil {
+      mp = make(map[int][]int)
+      me.lut[left] = mp
+   }
+
+   pair := mp[right]
+   if pair == nil {
+      pair = make([]int, 2)
+      mp[right] = pair
+   }
+
+   self := pair[OpMul]
+   if self == 0 {
+      self = len(me.list)
+      mul := &Mul{db: me, left: left, right: right, self: self}
+      me.list = append(me.list, mul)
+
+      pair[OpMul] = self
+   }
+   return self
 }
 
+func (me *Mul) NumOps() int {
+   return me.db.list[me.left].NumOps() + me.db.list[me.right].NumOps() + 1
+}
+
+func (me *Mul) Label() int {
+   return me.self
+}
+
+// Represents an addition
 type Add struct {
-   left, right Expr
+   db                *ExprDB
+   left, right, self int
 }
 
-func (me *Add) Equiv(e Expr) bool {
-   add, ok := e.(*Add)
-   if ok {
-      a := add.left.Equiv(me.right) && add.right.Equiv(me.left)
-      b := add.left.Equiv(me.left) && add.right.Equiv(me.right)
+func (me *ExprDB) NewAdd(left, right int) int {
 
-      return a || b
-   } else {
-      return false
+   if left > right {
+      left, right = right, left
    }
 
-   panic("bad return path error (x_X)")
+   mp := me.lut[left]
+   if mp == nil {
+      mp = make(map[int][]int)
+      me.lut[left] = mp
+   }
+
+   pair := mp[right]
+   if pair == nil {
+      pair = make([]int, 2)
+      mp[right] = pair
+   }
+
+   self := pair[OpAdd]
+   if self == 0 {
+      self = len(me.list)
+      add := &Add{db: me, left: left, right: right, self: self}
+      me.list = append(me.list, add)
+
+      pair[OpAdd] = self
+   }
+   return self
+}
+
+func (me *Add) NumOps() int {
+   return me.db.list[me.left].NumOps() + me.db.list[me.right].NumOps() + 1
+}
+
+func (me *Add) Label() int {
+   return me.self
 }
