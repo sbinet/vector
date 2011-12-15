76c2c52cf94e2b26d0ba2aa4bb6d18621ffc6803
Samuel C. Payson <scpayson@gmail.com>
2011-12-15 02:30:35 -0500

    Started adding astar.State capabilities.
    Soon '*Production' will implement 'astar.State'

--- codegen/state.go
+++ codegen/state.go
@@ -1,5 +1,7 @@
 package codegen
 
+import "astar"
+
 type Operand []int
 
 type Production struct {
@@ -8,15 +10,16 @@ type Production struct {
    Parent *Production
    Result Operand
    Op     *Operation
-   prob   *problemInstance
+   prob   *ProblemInstance
 
    // Depth of the operands that were passed to the operation to get this
    A, B int
 }
 
-type problemInstance struct {
+type ProblemInstance struct {
    goal *Operand
    db   *ExprDB
+   ops  []Operation
 }
 
 type Operation interface {
@@ -24,8 +27,22 @@ type Operation interface {
    String() string
 
    // Returns all Productions that could come after 'pred' using this
-   // operation. They must use 'pred.Result' as an operand.
-   Children(pred *Production) []Production
+   // operation. They must use 'pred.Result' as an operand. Note that
+   // '*Production' satisfies the 'astar.State' interface.
+   Children(pred *Production) []astar.State
+}
+
+
+
+func (me *Production) GetNext() []astar.State {
+   // Start with 16, since we're likely to get far more than that, but if we
+   // don't get that many it isn't too much wasted memory.
+   rv := make([]astar.State, 16)
+   for i := range me.prob.ops {
+      rv = append(rv, me.prob.ops[i].Children(me)...)
+   }
+
+   return rv
 }
 
 func (me *Production) IsGoal() bool {
