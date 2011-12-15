d629587af8639314df3eee84b793fcdd6fcb5533
Samuel C. Payson <scpayson@gmail.com>
2011-12-15 02:31:12 -0500

    Updated formatting.
    Forgot to run gofmt before committing 'codegen/expr_test.go'.

--- codegen/expr_test.go
+++ codegen/expr_test.go
@@ -2,20 +2,20 @@ package codegen
 
 import "testing"
 
-func TestExprDB( t * testing.T ) {
+func TestExprDB(t *testing.T) {
    db := NewExprDB()
 
    a, b, c := db.NewLiteral(), db.NewLiteral(), db.NewLiteral()
 
-   aPbXc := db.NewAdd( a, db.NewMul( b, c ) )
+   aPbXc := db.NewAdd(a, db.NewMul(b, c))
 
-   bXc := db.NewMul( b, c )
+   bXc := db.NewMul(b, c)
 
-   if aPbXc != db.NewAdd( a, bXc ) {
-      t.Errorf( "Did not generate same name for 'a + b * c' twice in a row" )
+   if aPbXc != db.NewAdd(a, bXc) {
+      t.Errorf("Did not generate same name for 'a + b * c' twice in a row")
    }
 
-   if aPbXc == db.NewAdd( a, db.NewMul( a, b ) ) {
-      t.Errorf( "Generated same name for different expressions" )
+   if aPbXc == db.NewAdd(a, db.NewMul(a, b)) {
+      t.Errorf("Generated same name for different expressions")
    }
 }
